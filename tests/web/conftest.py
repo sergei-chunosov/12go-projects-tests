6c1da80c2c43481743c963b0214122d181bc467b
sergei-chunosov <necros83@gmail.com>
2024-02-21 14:32:16 +0700

    add conftest

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -1,5 +1,8 @@
+import os
 import pytest
 from selene import browser
+from selenium import webdriver
+
 from utils import attach
 from dotenv import load_dotenv
 
@@ -20,33 +23,31 @@ def load_env():
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    # browser_version = request.config.getoption('--browser_version')
-    # browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
 
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": browser_version,
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-    #     options=options
-    # )
-    # browser.config.driver = webdriver
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = webdriver
 
     browser.config.base_url = "https://12go.asia/"
     browser.config.window_width = '1200'
     browser.config.window_height = '720'
     browser.config.timeout = 4
-    # cookies = get_cookie()
-    # browser.driver.add_cookie({'name': 'tuid', 'value': cookies})
 
     yield browser
 
