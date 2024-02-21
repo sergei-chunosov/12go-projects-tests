4a681e1ef294ac6dbac6b53ca42b712c0fc3202c
sergei-chunosov <necros83@gmail.com>
2024-02-21 15:12:36 +0700

    fix api tests

--- tests/API/test_wrong_auth.py
+++ tests/API/test_wrong_auth.py
@@ -4,7 +4,7 @@ from pages.API.auth import auth
 import allure
 
 @pytest.mark.api
-def test_auth():
-    with allure.step('Проверяем успешную аутентификацию через API'):
+def test_wrong_auth():
+    with allure.step('Проверяем аутентификацию с невалидными данными через API'):
         cookies = auth.get_cookie(user_name='ncrs@test.test', password='123456')
         assert cookies == None
